706cacca2ec277a18dd096e76f8c9a6497232a98
Richard-techno <richardluringsvensson@gmail.com>
2025-02-09 16:12:44 +0100

    Add files via upload
    add notepad

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let message = ["Hi there!", "Raito?", "Gonna code?", "Here to use ai?", "Good [insert time here]!", "Don't forget!", "Need a spark of creativity?"]
 let rand = Math.floor(Math.random() * 6)
+let note = document.getElementById('notepad');
+note.textContent = localStorage.getItem('note')
+note.addEventListener('change', save)
 document.getElementById('bt').textContent = message[rand]
+function save(){
+    localStorage.setItem('note', note.textContent)
+}
